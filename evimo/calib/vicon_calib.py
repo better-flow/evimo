8d35eb55d96fb14d3b3f1b9c28db58f13301ac92
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2020-03-19 17:25:23 -0400

    Add cam-to-vicon calibration script

---
+++ evimo/calib/vicon_calib.py
@@ -0,0 +1,226 @@
+#!/usr/bin/python3
+
+import numpy as np
+import cv2
+import glob
+import yaml
+import sys, os, math
+
+from pyquaternion import Quaternion
+from scipy.spatial.transform import Rotation
+
+class RigPoses:
+    def __init__(self, fname):
+        f = open(fname, 'r')
+        self.T = []
+        self.RPY = []
+        for line in f.readlines():
+            spl = line.split(' ')
+            tx = float(spl[1])
+            ty = float(spl[2])
+            tz = float(spl[3])
+            qx = float(spl[4])
+            qy = float(spl[5])
+            qz = float(spl[6])
+            qw = float(spl[7])
+
+            # invert rotation to enforce X' = R(X - T)
+            q = Quaternion(qw, qx, qy, qz).inverse
+            self.RPY.append(self.q2rpy(q))
+            self.T.append(np.array([[tx],[ty],[tz]]))
+
+    def calibrate(self, cam):
+        print(len(cam.img_T), len(self.RPY))
+        u0_arr = []
+        u1_arr = []
+        w_arr = []
+        R0_arr = []
+        R1_arr = []
+        T0_arr = []
+        T1_arr = []
+        for i in range(0, len(self.RPY) - 1):
+            if(i >= len(cam.img_T)): break
+            for j in range(i + 1, len(self.RPY)):
+                if(j >= len(cam.img_T)): break
+                R_, t_ = cam.get_Rt(i, j)
+                if (R_ is None or t_ is None): continue
+
+                R, t = self.get_Rt(self.T[i], self.RPY[i], self.T[j], self.RPY[j])
+                if (R is None or t is None): continue
+
+                u0 = R.as_rotvec()
+                u1 = R_.as_rotvec()
+                e = np.abs(np.linalg.norm(u0) - np.linalg.norm(u1))
+                if (e > 0.001): continue
+                if (np.linalg.norm(u0) < 0.2): continue
+                w_arr.append(e / np.linalg.norm(u0))
+                u0 /= np.linalg.norm(u0)
+                u1 /= np.linalg.norm(u1)
+
+                print(i, j, ':', u0, np.linalg.norm(R.as_rotvec()), u1, np.linalg.norm(R_.as_rotvec()))
+                print(t.transpose()[0], t_.transpose()[0])
+
+                u0_arr.append(u0)
+                u1_arr.append(u1)
+                R0_arr.append(R)
+                R1_arr.append(R_)
+                T0_arr.append(t)
+                T1_arr.append(t_)
+
+        u0 = np.array(u0_arr)
+        u1 = np.array(u1_arr)
+        w = np.array(w_arr)
+        w /= np.linalg.norm(w)
+
+        R, rmsd = Rotation.align_vectors(u0, u1, weights=w)
+
+        a = np.zeros(shape=(0,3), dtype=np.float)
+        b = np.zeros(shape=(0,1), dtype=np.float)
+        for i in range(len(R0_arr)):
+            R0 = R0_arr[i]
+            R1 = R1_arr[i]
+            T0 = T0_arr[i]
+            T1 = T1_arr[i]
+
+            a = np.vstack((a, R0.as_dcm() - np.eye(3)))
+            b = np.vstack((b, R.as_dcm() @ T1 - T0))
+
+        print (a.shape, b.shape)
+        T, res, rnk, s = np.linalg.lstsq(a, b, rcond=None)
+
+        print(R.as_dcm(), rmsd)
+        print(T.transpose()[0], res)
+        print()
+        print("x-y-z-R-P-Y:")
+        print(np.hstack((T.transpose()[0], R.as_euler('xyz'))))
+
+
+    # get R and T of the camera at time t=t1 in the frame of camera at t=t0
+    def get_Rt(self, T0, RPY0, T1, RPY1, dcm=False):
+        # looks like it performs 'zyx' order instead
+        R0 = Rotation.from_euler('xyz', RPY0, degrees=False)
+        R1 = Rotation.from_euler('xyz', RPY1, degrees=False)
+
+        R0_inv = R0.inv()
+        R = R1 * R0_inv # '*' instead of '@' because not numpy matrices
+
+        #T = np.expand_dims(R0.apply((T1 - T0).transpose()[0]), axis=0).transpose()
+        T = np.expand_dims(R1.apply((T0 - T1).transpose()[0]), axis=0).transpose()
+        if (dcm): R = R.as_dcm()
+        return R, T
+
+    def q2rpy(self, q):
+        w = q[0]
+        x = q[1]
+        y = q[2]
+        z = q[3]
+
+        X = math.atan2(2.0 * (w * x + y * z), 1.0 - 2.0 * (x * x + y * y))
+        sin_val = 2.0 * (w * y - z * x)
+        if (sin_val >  1.0): sin_val =  1.0
+        if (sin_val < -1.0): sin_val = -1.0
+
+        Y = math.asin(sin_val)
+        Z = math.atan2(2.0 * (w * z + x * y), 1.0 - 2.0 * (y * y + z * z))
+        return np.array([X, Y, Z])
+
+
+class Camera:
+    def __init__(self, calib, target):
+        self.name = calib['rostopic'].split('/')[1]
+        K = calib['intrinsics']
+        self.K = np.array([[K[0],0,K[2]],[0,K[1],K[3]],[0,0,1]], dtype=np.float32)
+        self.D = np.array(calib['distortion_coeffs'], dtype=np.float32)
+        self.res_x = calib['resolution'][0]
+        self.res_y = calib['resolution'][1]
+        self.dist_model = calib['distortion_model']
+        self.tgt_rows = target['targetRows']
+        self.tgt_cols = target['targetCols']
+        print(self.name, ':\n', self.K, '\n', self.D, '\n', self.dist_model, '\n')
+
+        self.objp = np.zeros((self.tgt_cols * self.tgt_rows, 3), np.float32)
+        self.objp[:,:2] = np.mgrid[0:self.tgt_cols,0:self.tgt_rows].T.reshape(-1,2)
+        self.objp[:,0] *= target['colSpacingMeters']
+        self.objp[:,1] *= target['rowSpacingMeters']
+
+        self.images = []
+        fnames = glob.glob(self.name + '/*.png')
+        ts = [int(os.path.basename(fname).split('.')[0]) for fname in fnames]
+        fnames = [n for _, n in sorted(zip(ts,fnames))]
+
+        for fname in fnames:
+            img = cv2.imread(fname)
+
+            if (self.dist_model == 'radtan'):
+                img = cv2.undistort(img, self.K, self.D, None, self.K)
+            elif (self.dist_model == 'equidistant'):
+                img = cv2.fishye.undistort(img, self.K, self.D, None, self.K)
+            else:
+                print("Unknown undistortion model!")
+
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray = 255 - gray
+
+            self.images.append(gray)
+
+        self.img_R = []
+        self.img_T = []
+        for i, img in enumerate(self.images):
+            ret, corners = cv2.findChessboardCorners(img, (self.tgt_cols, self.tgt_rows), None)
+            if (not ret):
+                self.img_R.append(None)
+                self.img_T.append(None)
+                continue
+            img_vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+            corners = cv2.cornerSubPix(img, corners, (11,11), (-1,-1), criteria)
+            ret_, rvecs, tvecs = cv2.solvePnP(self.objp, corners, self.K, np.zeros(4))
+
+            R,_ = cv2.Rodrigues(rvecs)
+            R = Rotation.from_matrix(R)
+            self.img_R.append(R)
+            self.img_T.append(tvecs)
+
+            print ("Added", fnames[i])
+            continue
+
+            axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
+            imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, self.K, np.zeros(4))
+            corner = tuple(corners[0].ravel())
+            img_vis = cv2.line(img_vis, corner, tuple(imgpts[0].ravel()), (0,0,255), 5)
+            img_vis = cv2.line(img_vis, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
+            img_vis = cv2.line(img_vis, corner, tuple(imgpts[2].ravel()), (255,0,0), 5)
+
+            img_vis = cv2.drawChessboardCorners(img_vis, (self.tgt_cols, self.tgt_rows), corners, ret)
+
+            cv2.imshow('img', img_vis)
+            cv2.waitKey(0)
+
+    def get_Rt(self, i0, i1, dcm=False):
+        T0 = self.img_T[i0]
+        T1 = self.img_T[i1]
+        R0 = self.img_R[i0]
+        R1 = self.img_R[i1]
+        if (T0 is None or T1 is None):
+            return None, None
+
+        R0_inv = R0.inv()
+        R = R1 * R0_inv
+
+        T = T1 - np.expand_dims(R.apply(T0.transpose()[0]), axis=0).transpose()
+        if (dcm): R = R.as_dcm()
+        return R, T
+
+
+
+target = yaml.load(open('target.yaml'), Loader=yaml.FullLoader)
+calib  = yaml.load(open('camchain-img.yaml'), Loader=yaml.FullLoader)
+rig_p = RigPoses('rig_0_poses.txt')
+for id_ in calib.keys():
+    cam = Camera(calib[id_], target)
+    rig_p.calibrate(cam)
+    break
+
+
+cv2.destroyAllWindows()
