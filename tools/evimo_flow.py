7576e2f72922b394073a713c60b6dcef1b6f102a
tobidelbruck <todelbru@ethz.ch>
2022-01-30 13:36:24 +0100

    improvements from converting GT flow for EDFLOW benchmarking. Output is compatible with MVSEC NPZ files now. Warnings printed on big timestamp gaps. Options changed to add --overwrite and --quiet. Command line accepts multiple files to convert. Timestamps in NPZ file are now in absolute time relative to epoch in double seconds, like MVSEC. Values in npy files are double values, like MVSEC. Output filenames are the same as MVSEC|.

--- tools/evimo_flow.py
+++ tools/evimo_flow.py
@@ -1,26 +1,42 @@
-###############################################################################
+#!/usr/bin/python
 #
-# File: evimo_flow_converter.py
+# ###############################################################################
+#
+# File: evimo_flow.py
 #
 # Calculate optical flow from EVIMO datasets
-# so far this is tested on EVIMO2
+# so far this is tested on EVIMO2 for VGA DVS reocrdings
 #
 # Usage:
-# python evimo_flow.py --file evimo_file.npz --dt 0.01
-#
-# Writes out flow files to:
-# evimo_file_flow.npz
-# which contains:
-# timestamps.npy
-# x_flow.npy
-# y_flow.npy
+# python evimo_flow.py  --dt 0.01 evimo_file.npz evimo_file2.npz ....
+# The source evimo_file.npz file(s) are one of the EV-IMO NPZ files that combine all the sensor and GT static pose data, e.g.
+# samsung_mono/imo/train/scene9_dyn_train_02.npz
+# The source NPZ file contents are documented in https://github.com/better-flow/evimo/wiki/Ground-Truth-Format
 #
 # "dt" is how far ahead of the camera trajectory to sample in seconds
 # when approximating flow through finite difference. Smaller values are
 # more accurate, but noiser approximations of optical flow.
 #
+# Writes out flow files to the NPZ file folder location as:
+# evimo_file_flow.npz
+# which contains:
+# timestamps.npy
+# x_flow_dist.npy
+# y_flow_dist.npy
+#
+# timestamps are relative to epoch time in double seconds
+# x_flow and y_flow are displacements in pixels between frames (timestamp intervals)
+#
+# Missing depth values (walls of room) are filled with NaN
+# The timestamps can skip values when the VICON has lost lock on one or more objects from occulusion or too rapid motion.
+#
+#
+# To use this converter standalone, make a python 3 environment and in it
+# pip install argparse numpy scipy opencv-python easygui tqdm
+#
 # History:
 # 01-21-22 - Levi Burner - Created file
+# 01-29-22 - Tobi Delbruck - corrected to match MVSEC NPZ flow output contents for mono camera recordings from MVSEC, added multiple file option
 #
 ###############################################################################
 
@@ -30,16 +46,26 @@ import numpy as np
 import pprint
 from scipy.spatial.transform import Rotation as R
 from scipy.spatial.transform import Slerp
+import easygui
+from tqdm import tqdm
+from pathlib import Path
+import statistics
 
 # Sample a list of translations and rotations
 # with linear interpolation
 def interpolate_cam_pose(t, cam_pose):
     right_i = np.searchsorted(cam_pose[:, 0], t)
+    if right_i==cam_pose.shape[0]:
+        print(f'attempted extrapolation past end of cam poses, clipping')
+        right_i=right_i-1 # prevent attempted extrapolation past array end
 
     left_t  = cam_pose[right_i-1, 0]
     right_t = cam_pose[right_i,   0]
 
     alpha = (t - left_t) / (right_t - left_t)
+    if alpha>1:
+        print('attempted alpha>1, clipping')
+        alpha=1
 
     left_position  = cam_pose[right_i - 1, 1:4]
     right_position = cam_pose[right_i,     1:4]
@@ -79,11 +105,12 @@ def project_points_radtan(points,
     return u, v
 
 # Generate an HSV image using color to represent the gradient direction in a optical flow field
-def visualize_optical_flow(flow):
+def visualize_optical_flow(flowin):
+    flow=np.ma.array(flowin, mask=np.isnan(flowin))
     theta = np.mod(np.arctan2(flow[:, :, 0], flow[:, :, 1]) + 2*np.pi, 2*np.pi)
 
     flow_norms = np.linalg.norm(flow, axis=2)
-    flow_norms_normalized = flow_norms / np.max(flow_norms)
+    flow_norms_normalized = flow_norms / np.max(np.ma.array(flow_norms, mask=np.isnan(flow_norms)))
 
     flow_hsv = np.zeros((flow.shape[0], flow.shape[1], 3), dtype=np.uint8)
     flow_hsv[:, :, 0] = 179 * theta / (2*np.pi)
@@ -92,17 +119,19 @@ def visualize_optical_flow(flow):
 
     return flow_hsv
 
-def draw_flow_arrows(img, xx, yy, flow_x, flow_y, p_skip=10):
+def draw_flow_arrows(img, xx, yy, dx, dy, p_skip=10):
     xx     = xx[::p_skip, ::p_skip].flatten()
     yy     = yy[::p_skip, ::p_skip].flatten()
     flow_x = dx[::p_skip, ::p_skip].flatten()
     flow_y = dy[::p_skip, ::p_skip].flatten()
 
     for x, y, u, v in zip(xx, yy, flow_x, flow_y):
+        if np.isnan(u) or np.isnan(v):
+            continue
         cv2.arrowedLine(img,
                         (int(x), int(y)),
-                        (int(x+u), int(y+v)),
-                        (255, 255, 255),
+                        (int(x+5*u), int(y+5*v)),
+                        (0, 0, 0),
                         tipLength=0.2)
 
 def flow_direction_image(shape=(60,60)):
@@ -152,18 +181,20 @@ def get_intrinsics(meta):
 
     return K, dist_coeffs
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--file', dest='file', help='EVIMO2 npz file to process',
-                         default='left_camera/imo/train/scene13_dyn_test_01.npz')
-    parser.add_argument('--dt', dest='dt', help='dt for flow approximation', type=float,
-                        default=0.01)
-    args = parser.parse_args()
 
-    file    = args.file
-    flow_dt = args.dt
+def convert(file, flow_dt, showflow=True, overwrite=False):
+    if file.endswith('_flow.npz'):
+        print(f'skipping {file} because it appears to be a flow output npz file')
+        return
 
-    data = np.load(file, allow_pickle=True)
+    npz_name_base=file[:-4] + '_flow'
+    p=Path(npz_name_base+'.npz')
+    if not overwrite and p.exists():
+        print(f'skipping {file} because {p} exists; use --overwrite option to overwrite existing output file')
+        return
+    print(f'converting {file} with dt={flow_dt}s; loading source...',end='')
+    data = np.load(file, allow_pickle=True, mmap_mode='r')
+    print('done loading')
     meta = data['meta'].item()
 
     cam_poses      = get_cam_poses (meta)
@@ -171,30 +202,39 @@ if __name__ == '__main__':
 
     # Get the map from pixels to direction vectors with Z = 1
     map1, map2 = cv2.initInverseRectificationMap(
-                        K, # Intrinsics
-                        dist_coeffs, # Distortion
-                        np.eye(3), # Rectification
-                        np.eye(3), # New intrinsics                 
-                        (data['depth'][0].shape[1], data['depth'][0].shape[0]),
-                        cv2.CV_32FC1)
+        K, # Intrinsics
+        dist_coeffs, # Distortion
+        np.eye(3), # Rectification
+        np.eye(3), # New intrinsics
+        (data['depth'][0].shape[1], data['depth'][0].shape[0]),
+        cv2.CV_32FC1)
 
     # Initial positions of every point
     x = np.arange(0, data['depth'][0].shape[1], 1)
     y = np.arange(0, data['depth'][0].shape[0], 1)
     xx, yy = np.meshgrid(x, y)
 
-    # For visualization
-    flow_direction_image_hsv = flow_direction_image((data['depth'][0].shape[0], data['depth'][0].shape[1]))
-    flow_direction_image_bgr = cv2.cvtColor(flow_direction_image_hsv, cv2.COLOR_HSV2BGR)
-    cv2.imshow('color direction chart', flow_direction_image_bgr)
+    if showflow:
+        # For visualization
+        flow_direction_image_hsv = flow_direction_image((data['depth'][0].shape[0], data['depth'][0].shape[1]))
+        flow_direction_image_bgr = cv2.cvtColor(flow_direction_image_hsv, cv2.COLOR_HSV2BGR)
+        cv2.imshow('color direction chart', flow_direction_image_bgr)
 
     # Preallocate arrays for flow as in MSEVC format
-    timestamps  = np.zeros((data['depth'].shape[0],), dtype=np.float32)
-    x_flow = np.zeros((data['depth'].shape), dtype=np.float32)
-    y_flow = np.zeros((data['depth'].shape), dtype=np.float32)
+    timestamps  = np.zeros((data['depth'].shape[0],), dtype=np.float64)
+    x_flow_dist = np.zeros((data['depth'].shape), dtype=np.float64) # named as in MVSEC monoocular camera flow, double as in MVSEC NPZs
+    y_flow_dist = np.zeros((data['depth'].shape), dtype=np.float64)
 
-    for i, (depth_frame_mm, frame_info) in enumerate(zip(data['depth'], meta['frames'])):
-        depth_mask = depth_frame_mm > 0
+    ros_time_offset=meta['meta']['ros_time_offset'] # get offset of start of data in epoch seconds to write the flow timestamps using same timebase as MVSEC
+    last_time=float('nan')
+    last_delta_time=float('nan')
+    delta_times=[]
+    start_time=float('nan')
+    large_delta_times=0
+
+    for i, (depth_frame_mm, frame_info) in enumerate(tqdm(zip(data['depth'], meta['frames']), total=len(timestamps))):
+        depth_mask = depth_frame_mm > 0 # if depth_frame_mm is zero, it means a missing value from lack of GT model (e.g. of room walls)
+        # these x_flow and y_flow values are filled with NaN
         depth_frame_float_m = depth_frame_mm.astype('float') / 1000.0
 
         # Initial point cloud
@@ -234,22 +274,64 @@ if __name__ == '__main__':
         # Calculate flow, mask out points where depth is unknown
         dx = (W_x - xx) * depth_mask
         dy = (W_y - yy) * depth_mask
+        dx[depth_mask==0]=float("nan")
+        dy[depth_mask==0]=float("nan")
 
-        timestamps[i] = left_time
-        x_flow[i, :, :] = dx
-        y_flow[i, :, :] = dy
+        relative_time=left_time
+        if not np.isnan(last_time):
+            this_delta_time=relative_time-last_time
+            delta_times.append(this_delta_time)
+            median_delta_time=statistics.median(delta_times)
+            if this_delta_time > 2*median_delta_time :
+                factor=this_delta_time/median_delta_time
+                print(f'Warning: This delta time {this_delta_time:.3f}s at relative time {relative_time:.3f}s is {factor:.1f}X more than the median delta time {median_delta_time:.3f}s')
+                large_delta_times+=1
+            last_delta_time=this_delta_time
+        else:
+            start_time=relative_time
+        last_time=relative_time
 
-        # Visualize
-        flow_hsv = visualize_optical_flow(np.dstack((dx, dy)))
-        flow_bgr = cv2.cvtColor(flow_hsv, cv2.COLOR_HSV2BGR)
+        timestamps[i] = relative_time +ros_time_offset
+        x_flow_dist[i, :, :] = dx
+        y_flow_dist[i, :, :] = dy
 
-        draw_flow_arrows(flow_bgr, xx, yy, dx, dy)
-        cv2.imshow('flow_bgr', flow_bgr)
+        if showflow:
+            # Visualize
+            flow_hsv = visualize_optical_flow(np.dstack((dx, dy)))
+            flow_bgr = cv2.cvtColor(flow_hsv, cv2.COLOR_HSV2BGR)
 
-        cv2.waitKey(1)
+            draw_flow_arrows(flow_bgr, xx, yy, dx, dy)
+            cv2.imshow('flow_bgr', flow_bgr)
 
+            cv2.waitKey(1)
+    del data
+    print(f'Relative start time {start_time:.3f}s, last time {last_time:.3f}s, duration is {(last_time-start_time):.3f}s.\n'
+          f'There are {large_delta_times} ({(100*(large_delta_times/len(timestamps))):.1f}%) excessive delta times.\n'
+          f'Saving {len(timestamps)} frames in NPZ file {npz_name_base}.npz...')
     np.savez_compressed(
-            file[:-4] + '_flow',
-            x_flow=x_flow,
-            y_flow=y_flow,
-            timestamps=timestamps)
+        npz_name_base,
+        x_flow_dist=x_flow_dist,
+        y_flow_dist=y_flow_dist,
+        timestamps=timestamps)
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dt', dest='dt', help='dt for flow approximation'
+          '"dt" is how far ahead of the camera trajectory to sample in seconds'
+        'when approximating flow through finite difference. Smaller values are'
+        ' more accurate, but noiser approximations of optical flow.', type=float,  default=0.01)
+    parser.add_argument('--quiet', help='turns off OpenCV graphical output windows', default=False, action='store_true')
+    parser.add_argument('--overwrite', dest='overwrite', action='store_true', help='Overwrite existing output files')
+
+    args,files = parser.parse_known_args()
+
+    if len(files)==0:
+        import easygui
+        files=[easygui.fileopenbox()]
+
+
+    for f in files:
+        convert(f, flow_dt=args.dt, showflow=not args.quiet, overwrite=args.overwrite)
