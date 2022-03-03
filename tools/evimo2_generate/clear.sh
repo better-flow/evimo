79a42bea755f872df7a48c1c6bd988aca18bc262
Levi Burner <leviburner@gmail.com>
2022-03-03 11:19:28 -0500

    Move generation scripts, add clear_all.sh, simplify generate_all.sh

--- tools/evimo2_generate/clear.sh
+++ tools/evimo2_generate/clear.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-
-echo "Folder: $1"
+echo "removing generated files from folder: $1"
 
 declare -a CameraArray=("flea3_7" "samsung_mono" "left_camera" "right_camera")
 for cam in ${CameraArray[@]}; do
-    echo $cam
-
     S_NAME=$(basename $1)
     VIS_FOLDER=$1/$cam/ground_truth/vis
     VIDEO_DST=$1/${S_NAME}_${cam}.mp4
     rm -rf $1/$cam/ground_truth
-    rm $VIDEO_DST
+
+    if [ -f $VIDEO_DST ] 
+    then
+        rm $VIDEO_DST
+    fi
 done
---
+++ tools/evimo2_generate/generate_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DATA_ROOT=$1
+
+# Get a sorted list of sequence folders
+folders=$(find $DATA_ROOT -mindepth 3 -maxdepth 3 -type d)
+folders=$(echo $folders | xargs -n1 | sort | xargs)
+folders=($folders)
+
+# Optionally use this hack to run just a few
+# folders=(
+# ./imo/eval/scene13_dyn_test_00
+# ./imo/eval/scene13_dyn_test_05
+# ./imo/eval/scene14_dyn_test_03
+# )
+
+for folder in "${folders[@]}"
+do
+    sequence_folder=$DATA_ROOT/$folder
+    bash ./generate.sh $sequence_folder
+done
